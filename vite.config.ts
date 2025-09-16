b420202d3866067f6246b1323264c0660f7662fc
marcusvml21 <marcusmarins@gmail.com>
2025-09-16 10:52:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "",   // 
+  base: "/", // 👈 Corrigido para funcionar com domínio próprio (GitHub Pages)
   server: {
     host: "::",
     port: 8080,
