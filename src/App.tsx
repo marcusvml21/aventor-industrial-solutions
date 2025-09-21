ccdaf83ef325b79471884486bd2612361fb0aaad
marcusvml21 <marcusmarins@gmail.com>
2025-09-21 13:16:10 -0300

    feat: QueryClientProvider do React Query reintroduzido

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+const queryClient = new QueryClient();
+
 const App = () => (
-  <TooltipProvider>
-    <HashRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </HashRouter>
-  </TooltipProvider>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <HashRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
