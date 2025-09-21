00c24ca24851f1424fdd2ef2c10eb4b22956263f
marcusvml21 <marcusmarins@gmail.com>
2025-09-21 12:18:18 -0300

    feat: adiciona HashRouter e rotas Index/NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <>
+  <HashRouter>
     <Header />
     <main>
-      <Index />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </main>
     <Footer />
-  </>
+  </HashRouter>
 );
 export default App;
