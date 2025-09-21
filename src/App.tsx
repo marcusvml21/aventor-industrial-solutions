1022115d2a974f589cd41618c2129e5698126245
marcusvml21 <marcusmarins@gmail.com>
2025-09-20 22:57:07 -0300

    test: força visualização básica para debug tela branca

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      {/* 👇 HashRouter garante funcionamento no GitHub Pages e domínio próprio */}
-      <HashRouter>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* 👇 Rota coringa para páginas inexistentes */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </HashRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return <div style={{fontSize:'32px',color:'green'}}>Site React Vite OK</div>;
+};
 
 export default App;
