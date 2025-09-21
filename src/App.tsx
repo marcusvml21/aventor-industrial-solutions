8542a48363d5c07d7e4328abed7001f6ad918845
marcusvml21 <marcusmarins@gmail.com>
2025-09-21 13:06:48 -0300

    feat: TooltipProvider reintroduzido

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <HashRouter>
-    <Header />
-    <main>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </main>
-    <Footer />
-  </HashRouter>
+  <TooltipProvider>
+    <HashRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </HashRouter>
+  </TooltipProvider>
 );
+
 export default App;
